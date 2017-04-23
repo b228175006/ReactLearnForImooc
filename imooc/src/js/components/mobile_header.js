779c6fa70747e2f22a39f7dd94947e6e999f0df8
野生程序员 <gamon1121@gmail.com>
2017-04-23 17:40:33 +0800

    mobile登录模块

--- imooc/src/js/components/mobile_header.js
+++ imooc/src/js/components/mobile_header.js
@@ -12,6 +12,12 @@ import {
 	Button,
 	Checkbox
 } from 'antd';
+import {
+	Router,
+	Route,
+	Link,
+	browserHistory
+} from 'react-router';
 const FormItem = Form.Item;
 const TabPane = Tabs.TabPane;
 const SubMenu = Menu.SubMenu;
@@ -54,18 +60,34 @@ class MobileHeader extends React.Component {
 		};
 		var formData = this.props.form.getFieldsValue();
 		console.log(formData);
-		var url = "http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=" + formData.userName + "&password=" + formData.password + "&r_userName=" + formData.r_userName + "&r_password=" + formData.r_password + "&r_confirmPassword=" + formData.r_confirmPassword;
+		var url = "http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action + "&username=" + formData.userName + "&password=" + formData.password + "&r_userName=" + formData.r_userName + "&r_password=" + formData.r_password + "&r_confirmPassword=" + formData.r_confirmPassword;
 		fetch(url, myFetchOptions).then(response => response.json()).then(json => {
 			this.setState({
 				userNickName: json.NickName,
 				userid: json.UserId
 			});
 		});
+		if (this.state.action == 'login') {
+			this.setState({
+				hasLogined: true
+			});
+		}
 		message.success('请求成功！');
 		this.setModalVisible(false);
 	};
 	login() {
 		this.setModalVisible(true);
+	};
+	callback(key) {
+		if (key == 1) {
+			this.setState({
+				action: 'login'
+			});
+		} else {
+			this.setState({
+				action: "register"
+			})
+		}
 	};
 	render() {
 		const {
@@ -84,29 +106,45 @@ class MobileHeader extends React.Component {
 					{userShow}
 				</header>
 				<Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible} onCancel={()=>this.setModalVisible(false)} onOk={()=>this.setModalVisible(false)} okText="关闭">
-							<Tabs type = "card">
-								<TabPane tab="注册" key="2">
-									<Form layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
-										<FormItem label="账号">
-											{getFieldDecorator('r_userName')(
-												<Input placeholder="请输入您的账号" />
-												)}
-										</FormItem>
-										<FormItem label="密码">
-											{getFieldDecorator('r_password')(
-												<Input type="password"  placeholder="请输入您的密码" />
-												)}
-										</FormItem>
-										<FormItem label="确认密码">
-											{getFieldDecorator('r_confirmPassword')(
-												<Input type="password" placeholder="请再次输入您的密码" />
-												)}
-										</FormItem>
-										<Button type="primary" htmlType="submit">注册</Button>
-									</Form>
-								</TabPane>
-							</Tabs>
-						</Modal>
+					<Tabs type = "card" onChange={this.callback.bind(this)}>
+						<TabPane tab="登录" key="1">
+							<Form layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
+								<FormItem label="账号">
+									{getFieldDecorator('userName')(
+										<Input placeholder="请输入您的账号" />
+										)}
+								</FormItem>
+								<FormItem label="密码">
+									{getFieldDecorator('password')(
+										<Input type="password"  placeholder="请输入您的密码" />
+										)}
+								</FormItem>
+								<Button type="primary" htmlType="submit">登录</Button>
+							</Form>
+						</TabPane>
+
+						<TabPane tab="注册" key="2">
+							<Form layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
+								<FormItem label="账号">
+									{getFieldDecorator('r_userName')(
+										<Input placeholder="请输入您的账号" />
+										)}
+								</FormItem>
+								<FormItem label="密码">
+									{getFieldDecorator('r_password')(
+										<Input type="password"  placeholder="请输入您的密码" />
+										)}
+								</FormItem>
+								<FormItem label="确认密码">
+									{getFieldDecorator('r_confirmPassword')(
+										<Input type="password" placeholder="请再次输入您的密码" />
+										)}
+								</FormItem>
+								<Button type="primary" htmlType="submit">注册</Button>
+							</Form>
+						</TabPane>
+					</Tabs>
+				</Modal>
 			</div>
 		);
 	};
