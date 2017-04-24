1ea137ac4b2162441050b581986b55aaf6d214f4
野生程序员 <gamon1121@gmail.com>
2017-04-24 22:15:49 +0800

    移动端导航条
    更新了pc端样式，增加了移动端导航条

--- imooc/src/js/components/mobile_index.js
+++ imooc/src/js/components/mobile_index.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {
+	Tabs
+} from 'antd';
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
+
+const TabPane = Tabs.TabPane;
 export default class MobileIndex extends React.Component {
+
 	render() {
 		return (
 			<div>
 				<MobileHeader></MobileHeader>
+				<Tabs>
+					<TabPane key = "1" tab="头条"></TabPane>
+					<TabPane key = "2" tab="社会"></TabPane>
+					<TabPane key = "3" tab="国内"></TabPane>
+					<TabPane key = "4" tab="国际"></TabPane>
+					<TabPane key = "5" tab="娱乐"></TabPane>
+					<TabPane key = "6" tab="体育"></TabPane>
+					<TabPane key = "7" tab="科技"></TabPane>
+					<TabPane key = "8" tab="时尚"></TabPane>
+				</Tabs>
 				<MobileFooter></MobileFooter>
 			</div>
 		);
